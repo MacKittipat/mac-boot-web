ddce15a8d96867e0d66cc2be7357aa44c34cb8a5
mac <mac.kittipat@gmail.com>
2019-02-02 10:05:04 +0700

    Create controller

---
+++ src/main/java/com/mackittipat/macbootweb/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macbootweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
